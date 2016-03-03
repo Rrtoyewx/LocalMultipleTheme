02a71929ad6ae4161b023183a17fbaeae820bd60
Rrtoywx <nianting512@qq.com>
2016-03-03 18:18:37 +0800

    1 new stample

--- app/src/main/java/com/idreamo/rrtoyewx/localmultipletheme/MainActivity.java
+++ app/src/main/java/com/idreamo/rrtoyewx/localmultipletheme/MainActivity.java
@@ -1,9 +1,13 @@
 package com.idreamo.rrtoyewx.localmultipletheme;
 
+import android.app.Activity;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -14,51 +18,74 @@ import com.idreamo.rrtoyewx.multiplethemelibrary.multipleattrs.TextColorAttr;
 import com.idreamo.rrtoyewx.multiplethemelibrary.multipleattrs.TextSizeAttr;
 import com.idreamo.rrtoyewx.multiplethemelibrary.multipleitems.MultipleItem;
 
-public class MainActivity extends AppCompatActivity {
-    private TextView textView;
-    private RelativeLayout relativeLayout;
-    private boolean isNight;
+public class MainActivity extends Activity {
+    private Activity mActivity;
 
+    private Button mDayBtn;
+    private Button mNightBtn;
+    private ImageView mShowImage;
+    private RelativeLayout mBackLayout;
+    private TextView mContentText;
+
+    private boolean isNight;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MultipleTheme.init();
+        mActivity = this;
 
-        textView = (TextView) findViewById(R.id.tv);
-        relativeLayout = (RelativeLayout) findViewById(R.id.rl);
+        mDayBtn = (Button) findViewById(R.id.btn_day_mode);
+        mNightBtn = (Button) findViewById(R.id.btn_night_mode);
+        mShowImage = (ImageView) findViewById(R.id.iv_show);
+        mBackLayout = (RelativeLayout) findViewById(R.id.rl);
+        mContentText = (TextView) findViewById(R.id.tv_content);
 
+        generateMultipleItems();
 
-//        ColorAttr backgroudAttr = new ColorAttr(textView, R.attr.background_color, this);
-//        TextColorAttr textColorAttr = new TextColorAttr(textView, R.attr.text_color, this);
-//        TextSizeAttr textSizeAttr = new TextSizeAttr(textView, R.attr.text_size, this);
-        MultipleItem multipleItem = new MultipleItem.MultipleItemBuilder().setTargetView(textView)
-                .addAttrs(new TextColorAttr(textView, R.attr.text_color, this))
-                .addAttrs(new ColorAttr(textView, R.attr.background_color, this))
-                .addAttrs(new TextSizeAttr(textView, R.attr.text_size, this))
+        bindEvent();
+
+    }
+
+    private void generateMultipleItems() {
+        MultipleItem textItem = new MultipleItem.MultipleItemBuilder()
+                .setTargetView(mContentText)
+                .addAttrs(new TextColorAttr(mContentText,R.attr.text_color,this))
+                .addAttrs(new TextSizeAttr(mContentText,R.attr.text_size,this))
                 .create();
 
-
-        DrawAttr backgroudAttr1 = new DrawAttr(relativeLayout, R.attr.background_drawable, this);
-
-
         MultipleTheme.newSingleInstance()
-                .addMultipleItem(multipleItem)
-                .addMultipleItem(relativeLayout, true, backgroudAttr1);
+                .addMultipleItem(mBackLayout,new ColorAttr(mBackLayout,R.attr.background_color,this))
+                .addMultipleItem(mShowImage,new DrawAttr(mShowImage,R.attr.image_backgroud,this))
+                .addMultipleItem(textItem);
+    }
 
 
-        textView.setOnClickListener(new View.OnClickListener() {
+    private void bindEvent() {
+        mDayBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (isNight) {
-                    MainActivity.this.setTheme(R.style.DayTheme);
-                    MultipleTheme.newSingleInstance().startDispatchChangeTheme(R.style.DayTheme);
-                } else {
-                    MainActivity.this.setTheme(R.style.NightTheme);
-                    MultipleTheme.newSingleInstance().startDispatchChangeTheme(R.style.NightTheme);
-                }
-                isNight = !isNight;
+                isNight = false;
+                mActivity.setTheme(R.style.DayTheme);
+                MultipleTheme.newSingleInstance().startDispatchChangeTheme(R.style.DayTheme);
             }
         });
 
+        mNightBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isNight = true;
+                mActivity.setTheme(R.style.NightTheme);
+                MultipleTheme.newSingleInstance().startDispatchChangeTheme(R.style.NightTheme);
+            }
+        });
+
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        MultipleTheme.newSingleInstance().clearAll();
+        super.onDestroy();
     }
 }
